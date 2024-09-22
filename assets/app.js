c3de9d92e030e285fdef747f96fa6dc4ac4270b1
mareola <bulun1001@gmail.com>
2024-09-22 01:52:12 +0100

    -Update- Finished Section one of Javascript Turotial Video

---
+++ assets/app.js
@@ -0,0 +1,79 @@
+// Naming Variables
+var principal = 4000
+var rate = 0.5
+var time = 2
+
+// How to console
+console.log("The SI is"+"\n "+(principal * rate * time)/100)
+
+// Data types
+a = 5
+b = 5
+c = "5"
+
+// If statement
+if (a === c) {
+    console.log("true")
+} else {
+    console.log("false")
+
+}
+
+var age = 25
+var name = 20
+
+if (age < 25){
+    console.log("Not allowed to register")
+}
+
+if (age < 18){
+    console.log("Less than 18")
+}
+else{
+    console.log("Allowed")
+}
+
+var i = 35
+
+if (i < 40 && i > 30){
+    console.log("Age is mid")
+}
+
+// Switch statement
+var j = 200
+switch (true){
+
+    case j == 200:
+        console.log("J is 200")
+
+    case j >= 100:
+        console.log("it is greater")
+
+    break;
+
+    default:
+        console.log("J is something else")
+}
+
+// Loops
+    // For Loop
+for (k = 5; k >= 0; k--){
+    switch(true){
+        case k % 2 == 0:
+            console.log(k)
+        break;
+        default:
+    }
+}
+
+
+    // While Loop
+var number = prompt("Enter Your Age")
+    while (number == ""){
+        alert("You did Not Choose a Number")
+        var number = prompt("Enter Your Number")
+    }
+
+    if (number > 18){
+        alert("Congrats")
+    }
